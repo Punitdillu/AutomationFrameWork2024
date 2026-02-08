7a6fe6159bcb15652388367a329f9a19f5b58afb
Punitdillu <punitranjan2105@gmail.com>
2026-02-08 14:44:36 +0530

    switch implemented

--- DynamicAutomation/src/main/java/GenericUtility/BaseClass.java
+++ DynamicAutomation/src/main/java/GenericUtility/BaseClass.java
@@ -38,24 +38,23 @@ public class BaseClass {
     public void LaunchBrowser() throws Throwable {
 		String browserName= file.getCommonProperty("browser");
 		//String browserName
-		if(browserName.equals("chrome"))
-		{
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
-		}
-		else if(browserName.equals("firefox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
-		}
-		else if(browserName.equals("edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver=new EdgeDriver();
-		}
-		else {
-			throw new Exception("not compatible browser");
+        switch (browserName) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new Exception("not compatible browser");
 
-		}
+        }
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		sdriver=driver;
 	}
